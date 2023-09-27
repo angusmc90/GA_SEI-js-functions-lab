6b17b5fedf90f34e299bd7a0fc0d33ab4cabbd22
angusmc90 <angusmc90@gmail.com>
2023-09-27 14:56:58 -0500

    end of lesson

--- js/app.js
+++ js/app.js
@@ -29,3 +29,21 @@ sayGoodbye("Adios", "Laura");
 sayGoodbye("Chao", "Becky");
 sayGoodbye("Goodbye", "Chad");
 sayGoodbye("Peace", "Frankie");
+
+
+function add (x,y){
+    return x + y
+}
+
+console.log(add(2,4));
+
+
+
+function getPlayerUserName(username){
+    username.toUpperCase()
+};
+
+playerOne = getPlayerUserName()
+playerTwo = getPlayerUserName()
+playerThree = getPlayerUserName()
+playerFour = getPlayerUserName()
