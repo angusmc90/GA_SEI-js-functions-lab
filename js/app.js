238b68d148044e99dd8ce84f13568309a176ebca
angusmc90 <angusmc90@gmail.com>
2023-09-27 15:29:05 -0500

    function number 2 solved?

--- js/app.js
+++ js/app.js
@@ -75,6 +75,18 @@ function maxOfTwoNumbers(x, y) {
 
 //2. Define a function, as a function expression, maxOfThree that takes three numbers as arguments and returns the largest of them. Again, the Math.max method is not allowed.
 
+function maxOfThree(num1, num2, num3) {
+    if (num1 > num2 && num1 > num3){
+        return num1
+    } else if (num2 > num1 && num2 > num3) {
+        return num2
+    } else {
+        return num3
+    }
+}
+
+console.log(maxOfThree(4, 5, 6));
+
 //3. Define a function, as a function declaration, isCharAVowel that takes a character as an argument and returns true if it is a vowel, false otherwise.
 
 //4. Define a function, as a function expression, sumArray that takes an array of numbers and returns the sum of those numbers. For example, sumArray([2, 4, 5]); would return 11.
