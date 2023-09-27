a9ea6a47775023255fb269a9035eb98670873cfb
angusmc90 <angusmc90@gmail.com>
2023-09-27 14:01:32 -0500

    adding function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 console.log("My name is Angus")
+
+
+// defining a function
+// declaring a function
+// creating the reusable block of code 
+// to be used at a later time in the program
+function sayHello(){
+	// write the code to say hello here
+    console.log("Hello world!")
+}
+
+sayHello();
